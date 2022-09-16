f2eacdfdfee5d8be08e60769e902dc4fb596ab14
xy <mafario_dh@163.com>
2022-09-15 22:44:28 -0700

    Add static banner in home page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Banner from './Banner.js'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save reload.
         </p>
@@ -16,7 +17,8 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
+      <Banner />
       </header>
     </div>
   );
---
+++ src/Banner.js
@@ -0,0 +1,12 @@
+import { Button, ButtonGroup, Dropdown } from "react-bootstrap";
+import './Banner.css';
+
+function Banner() {
+  return (
+    <div className="Banner">
+      <h1 className="display-1 mb-3 fw-lighter">Welcome to Board4Fun!</h1>
+    </div>
+  );
+}
+
+export default Banner;
